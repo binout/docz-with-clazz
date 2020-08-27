42731d6fc27145bfa5ea02d2c65c81e4f3eac06e
b.prioux <b.prioux@lectra.com>
2020-08-27 11:13:02 +0200

    setup docz

--- example/src/ProgressFoo.tsx
+++ example/src/ProgressFoo.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 // tag::props[]
-type ProgressFooProps = {
-  // value of progress, default is 50
-  now?: number 
+interface ProgressFooProps {
+  /** value of progress (min=0, max=100, default is 50) */
+  now?: number
 }
 // end::props[]
 
-const ProgressFoo: React.FC<ProgressFooProps> = ({ now = 50 }) => {
-  return (<div>{now}</div>)
-};
+const ProgressFoo = ({ now = 50 }: ProgressFooProps) =>
+    <h1>{now}</h1>
 
 export default ProgressFoo;
--- example/src/index.ts
+++ example/src/index.ts
@@ -1,3 +1,3 @@
-import BasicButton from "./ProgressFoo";
+import ProgressFoo from "./ProgressFoo";
 
-export default BasicButton;
+export default ProgressFoo;
