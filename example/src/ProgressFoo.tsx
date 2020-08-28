0b4f01f2d598bfe5b4d295f4f1a7b66bdfd18d2e
b.prioux <b.prioux@lectra.com>
2020-08-28 16:12:37 +0200

    add styled component

--- example/src/ProgressFoo.tsx
+++ example/src/ProgressFoo.tsx
@@ -1,16 +1,40 @@
 import React from "react";
-import './ProgressFoo.css';
+import styled from 'styled-components';
 
 // tag::props[]
 interface ProgressFooProps {
   /** value of progress (min=0, max=100, default is 50) */
-  now?: number
+  now?: number,
+  /** variant which defines color (default is info) */
+  variant?: 'success' | 'info' | 'warning' | 'danger',
+  /** set to true to display a label next to progress bar (default is false) */
+  label?: boolean
 }
 // end::props[]
 
-const ProgressFoo = ({ now = 50 }: ProgressFooProps) => {
+const ProgressBar = styled.progress`
+  -webkit-appearance: none;
+  ::-webkit-progress-value { 
+    background-color: ${props => props.color}
+  } 
+  ::-moz-progress-bar {
+    background-color: ${props => props.color}
+  }
+`;
+
+const ProgressFoo = ({ now = 50, variant = 'info', label = false }: ProgressFooProps) => {
+  let color
+  switch (variant) {
+    case 'info': color = 'blue'; break;
+    case 'success': color = 'green'; break;
+    case 'warning': color = 'orange'; break;
+    case 'danger': color = 'red'; break;
+  }
   return (
-    <progress value={now} max="100"></progress>
+    <div>
+      <ProgressBar value={now} max="100" color={color}></ProgressBar>
+      {label ? <span>{now}%</span> : ''}
+    </div>
   )
 }
 
